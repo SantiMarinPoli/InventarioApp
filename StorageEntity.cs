693d423d11136d3066b8657f88b93c1143ce6a1a
Santiago Cortes <santi.lamarin98@gmail.com>
2022-04-05 21:55:49 -0500

    Realizando modelo en las clases en el proyecto inventario en la clase  de platzi

---
+++ StorageEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Entities
+{
+    public class StorageEntity
+    {
+        [Key]
+        [StringLength(50)]
+        public string StorageId { get; set; }
+        [Required]
+        public DateTime LastUpdate { get; set; }
+        public int PartialQuantity { get; set; }
+        public string ProductId { get; set; }
+        public ProductEntity Product { get; set; }
+        public ICollection<InOutEntity> InOuts { get; set; }
+        public string WarehouseId { get; set; }
+        public WarehouseEntity Warehouse { get; set; }
+    }
+}
